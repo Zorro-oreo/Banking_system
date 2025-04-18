98fac110b7d23fc406a89c7e0d9f278d267603e4
Youssef Amgad <youssof_private@outlook.com>
2025-04-19 00:12:16 +0200

    Client and UI done

--- Client.cpp
+++ Client.cpp
@@ -22,3 +22,39 @@ void Client::deleteAccount(string ID) {
 		cout << "Account not found!" << endl;
 	}
 }
+
+void Client::addaccount(string type) {
+   while (true) {  
+       if (type != "Savings" && type != "Current") {  
+           cout << "Invalid account type!" << endl;  
+           return;  
+       }  
+       string ID = to_string(rand() % 1000000000000);  
+       if (ID.length() < 12) {  
+           ID.insert(0, 12 - ID.length(), '0');  
+       }  
+       bool isUnique = true;  
+       for (int i = 0; i < accounts.size(); i++) {  
+           if (accounts[i].getID() == ID) {  
+               isUnique = false;  
+               break;  
+           }  
+       }  
+       if (isUnique) {  
+           Accounts newAccount(type, ID);  
+           accounts.push_back(newAccount);  
+           cout << "Account added successfully with ID: " << ID << endl;  
+           return;  
+       }  
+   }  
+}
+void Client::displayAccounts() {
+	if (accounts.empty()) {
+		cout << "No accounts." << endl;
+		return;
+	}
+	for (int i = 0; i < accounts.size(); i++) {
+		accounts[i].displayAccount();
+	
+	}
+}
--- UI.cpp
+++ UI.cpp
@@ -2,6 +2,7 @@
 #include "UI.h"
 #include "User.h"
 #include "Admin.h"
+#include "Client.h"
 #include "Accounts.h"
 
 User UI::current_user;
@@ -18,10 +19,17 @@ void UI::main_menu() {
 	switch (c) {
 	case 1:
 		login();
-		break;
+		if (current_user.get_type() == "Admin") {
+			admin_menu();
+		}
+		else if (current_user.get_type() == "Client") {
+			client_menu();
+		}
 	case 2:
 		sign_up();
-		break;
+		cout << "Sign up successful. Please login.\n";
+		main_menu();
+		
 	case 3:
 		cout << "Exiting...\n";
 		exit(0);
@@ -33,43 +41,83 @@ void UI::login() {
 	cin >> username;
 	cout << "Enter password: " << endl;
 	cin >> password;
+	bool logged_in = false;
+
 	for (int i = 0; i < users.size(); i++) {
-
-		bool logged_in = false;
-
 		if (users[i].get_username() == username && users[i].get_password() == password) {
-			current_user = users.at(i);
+			current_user = users[i];
 			cout << "Login successful!\n";
 			logged_in = true;
 			break;
 		}
-
-		if (!logged_in) {
-			cout << "Invalid username or password. Please try again.\n";
-			main_menu();
-		}
-
+	}
+	if (!logged_in) {
+		cout << "Invalid username or password. Please try again.\n";
+		main_menu();
 	}
 }
 void UI::sign_up() { //Main menu. Anyone can do it...
-	string username, password;
+	string username, password, id;
 	cout << "Enter username: " << endl;
 	cin >> username;
+	for (int i = 0; i < users.size(); i++) {
+		if (users[i].get_username() == username) {
+			cout << "Username already exists. Please try again.\n";
+			sign_up();
+		}
+	}
 	cout << "Enter password: " << endl;
 	cin >> password;
-	User new_user(username, password, "Client", "123456789");
+	while (true) {
+		id = to_string(rand() % 1000000000000);
+		if (id.length() < 12) {
+			id.insert(0, 12 - id.length(), '0');
+		}
+		bool isUnique = true;
+		for (int i = 0; i < users.size(); i++) {
+			if (users[i].get_id() == id) {
+				isUnique = false;
+				break;
+			}
+		}
+		if (isUnique) {
+			break;
+		}
+	}
+	User new_user(username, password, "Client", id);
 	users.push_back(new_user);
-	cout << "Sign up successful.\n";
 }
 
 void UI::add_admin() { //Only accessed when the current_user is an admin...
 
-	string username, password;
+	string username, password, id;
 	cout << "Enter username: " << endl;
 	cin >> username;
+	for (int i = 0; i < users.size(); i++) {
+		if (users[i].get_username() == username) {
+			cout << "Username already exists. Please try again.\n";
+			sign_up();
+		}
+	}
 	cout << "Enter password: " << endl;
 	cin >> password;
-	User new_user(username, password, "Admin", "123456788");
+	while (true) {
+		id = to_string(rand() % 1000000000000);
+		if (id.length() < 12) {
+			id.insert(0, 12 - id.length(), '0');
+		}
+		bool isUnique = true;
+		for (int i = 0; i < users.size(); i++) {
+			if (users[i].get_id() == id) {
+				isUnique = false;
+				break;
+			}
+		}
+		if (isUnique) {
+			break;
+		}
+	}
+	User new_user(username, password, "Admin", id);
 	users.push_back(new_user);
 	cout << "New admin added.\n";
 
@@ -79,24 +127,43 @@ void UI::admin_menu() {
 	
 	while (true) {
 		int choice;
-		cout << "\n1. Display Admins\n2. Add New Admin\n3. Display Clients\n4.Remove User \n5. Log out\n6. Exit\nEnter choice (1, 2, 3, 4, 5, 6): ";
+		cout << "\n1. Display Admins\n2. Add New Admin\n3. Display Clients\n4. Remove User \n5. Log out\n6. Exit\nEnter choice (1, 2, 3, 4, 5, 6): ";
 		cin >> choice;
-		switch (choice) {
-		case 1:
-			admin.displayUsers(&users);
-			break;
-		case 2:
+		if (choice == 1) {
+			for (int i = 0; i < users.size(); i++) {
+				if (users[i].get_type() == "Admin") {
+					display_user(users[i]);
+				}
+			}
+		}
+		else if (choice == 2) {
 			add_admin();
-			break;
-		case 3:
-			admin.displayUsers(&users);
-			break;
-		case 4:
+		}
+		else if (choice == 2) {
+			add_admin();
+		}
+		else if (choice == 3) {
+			for (int i = 0; i < users.size(); i++) {
+				if (users[i].get_type() == "Client") {
+					display_user(users[i]);
+				}
+			}
+		}
+		else if (choice == 4) {
 			string id;
 			cout << "Enter user ID to remove: ";
 			cin >> id;
 			admin.removeUser(&id);
-			break;
+		}
+		else if (choice == 5) {
+			main_menu();
+		}
+		else if (choice == 6) {
+			cout << "Exiting...\n";
+			exit(0);
+		}
+		else {
+			cout << "Invalid choice. Please try again.\n";
 		}
 
 	}
@@ -110,4 +177,40 @@ void UI::display_user(User usr) {
 }
 vector <User> UI::get_users() {
 	return users;
+}
+
+void UI::client_menu() {
+	Client client(current_user.get_username(), current_user.get_password(), current_user.get_type(), current_user.get_id());
+
+	while (true) {
+		int choice;
+		cout << "\n1. Add Account\n2. Delete Account\n3. Display Accounts\n4. Log out\n5. Exit\nEnter choice (1, 2, 3, 4, 5): ";
+		cin >> choice;
+		if (choice == 1) {
+			string type;
+			cout << "Enter account type (Savings/Current): ";
+			cin >> type;
+			client.addaccount(type);
+		}
+		else if (choice == 2) {
+			string ID;
+			cout << "Enter account ID to delete: ";
+			cin >> ID;
+			client.deleteAccount(ID);
+		}
+		else if (choice == 3) {
+			cout << "Accounts: " << endl;
+			client.displayAccounts();
+		}
+		else if (choice == 4) {
+			main_menu();
+		}
+		else if (choice == 5) {
+			cout << "Exiting...\n";
+			exit(0);
+		}
+		else {
+			cout << "Invalid choice. Please try again.\n";
+		}
+	}
 }
